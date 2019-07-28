8d5ba7357d906357ae6855c1b92e5449c8845754
Nis Meinert <mail@nismeinert.de>
2019-07-28 16:17:52 +0200

    start implementing unittest for binary overlap PCA

--- test/test_pipelines.py
+++ test/test_pipelines.py
@@ -6,7 +6,7 @@ import test.pipelines
 import test.steps
 from test.helper import fit, transform, mat_eq
 from wpipln.pipelines import Pipeline, BalancedPipeline
-from wpipln.steps import BaseStep, Standardize, PCA
+from wpipln.steps import BaseStep, Standardize, PCA, BinaryOverlapPCA
 
 
 class TestPipeline(unittest.TestCase):
@@ -422,6 +422,37 @@ class TestPCAPipeline(unittest.TestCase):
 
         for std1, std2 in zip(stds1, stds2):
             self.assertAlmostEqual(std1, std2)
+
+
+class TestBinaryOverlapPCA(unittest.TestCase):
+    def test_ordering(self):
+        pipeline = Pipeline()
+        pipeline.add_step('std', BinaryOverlapPCA())
+
+        mean1 = [.5, -.5]
+        mean2 = [-.5, .5]
+        cov = [[1., .9], [.9, 1.]]
+        X1 = np.random.multivariate_normal(mean1, cov, 10000)
+        X2 = np.random.multivariate_normal(mean2, cov, 10000)
+
+        X = np.vstack((X1, X2))
+        y = np.append(np.zeros(10000), np.ones(10000))
+        w = np.ones(20000)
+        pipeline.fit(X, y, w)
+
+        Xt, _, _ = pipeline.transform(X, y, w)
+        is_diag = lambda X: np.allclose(X - np.diag(np.diagonal(X)), np.zeros(X.shape))
+        self.assertTrue(is_diag(np.cov(Xt)))
+
+        sel1 = (y == 0)
+        sel2 = (y == 1)
+        overlap1 = BinaryOverlapPCA.overlap(x1=Xt[sel1, 0], w1=w[sel1], x2=Xt[sel2, 0], w2=w[sel2],
+                                            bin_edges=np.linspace(-5, 5, 101))
+        overlap2 = BinaryOverlapPCA.overlap(x1=Xt[sel1, 1], w1=w[sel1], x2=Xt[sel2, 1], w2=w[sel2],
+                                            bin_edges=np.linspace(-5, 5, 101))
+
+        self.assertGreater(overlap1, .9)
+        self.assertLess(overlap2, .1)
 
 
 if __name__ == '__main__':
